7e1740e06be2dbec1b303e4a18ff338cc8b02e50
Sakthivel Karthikeyan <32382572+myselfsakthivel@users.noreply.github.com>
2022-11-23 20:48:41 +0530

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,10 +1,16 @@
 const { test } = require("./test");
-const { getColorFromPairNumber } = require("./color-coder");
+const { MajorColorNames, MinorColorNames } = require("./color-coder");
 
 function printColorCodes() {
     for (i = 1; i < 26; i++) {
-        let colorPair = getColorFromPairNumber(i);
-        console.log(`${i}:${colorPair}`);
+        let minorSize = MajorColorNames.length;
+        let majorSize = MinorColorNames.length;
+        let zeroBasedPairNumber = i - 1;
+        let majorIndex = parseInt(zeroBasedPairNumber / minorSize);
+        let minorIndex = parseInt(zeroBasedPairNumber % minorSize);
+        pair.majorColor = MajorColorNames[majorIndex];
+        pair.minorColor = MinorColorNames[minorIndex];
+        console.log(`${i}:[${MajorColorNames[majorIndex]},${MinorColorNames[minorIndex]}`);
     }
 }
 
