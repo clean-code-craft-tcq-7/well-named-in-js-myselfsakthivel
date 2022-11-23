52e5f61abdae8346ac4701621d3c5b97c5bbd00b
Sakthivel Karthikeyan <32382572+myselfsakthivel@users.noreply.github.com>
2022-11-23 20:35:57 +0530

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,3 +1,12 @@
 const { test } = require("./test");
+const { getColorFromPairNumber } = require("./color-coder");
+
+function printColorCodes() {
+    for (i = 1; i < 26; i++) {
+        let colorPair = getColorFromPairNumber(i);
+        console.log(`${i}:${colorPair}`);
+    }
+}
 
 test();
+printColorCodes();
